cc6c7a566bb67e592e69d77700bbfe76d48dd5ca
Sirendium <Sirendium@gmail.com>
2020-10-04 22:52:25 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IQ Dashboard</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Styles/background_interface.css" type="text/css" media="screen, projection">
+</head>
+<body>
+<div id="logining_page">
+<div id="footer">
+	<a href="index.php"><img src="Custom_files/Logo_white_TM.png" width="20%" height="10%" id="logo"></a><a id="BigHeder">Bysec security technology trade mark</a> 
+	<a  href="detects.php"><img src="Custom_files/g1.png" id="menu"></a>
+	<a href="monitoring.php"><img src="Custom_files/g2.png" id="menu"></a>
+	<a href="radar.php"><img src="Custom_files/g3.png" id="menu"></a>
+	<a href="cctv.php"><img src="Custom_files/g4.png" id="menu"></a>
+<div id="content" >
+<form method="POST">
+	<h1 id="Zaglavie">Login</h1>
+	<a id="BigHeder">Username </a><input type="text" name="Username"><br>
+	<a id="BigHeder">Email  &#160; &#160; &#160; &#160; </a><input type="text" name="EMAIL"><br>
+	<a id="BigHeder">Password </a><input type="text" name="PASSWORD"><br><br>
+	<input type="submit" name="logining" value="Come to IQ DASHBOARD" id="button_login" >
+</form>
+</div >
+<p id="InfoBox">© 2020 BYSEC</p>
+</div>
+
+</div>
+
+</body>
+</html>
---
+++ radar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IQ Dashboard</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Styles/background_interface.css" type="text/css" media="screen, projection">
+	<link rel="stylesheet" href="Styles/background_interface1.css" type="text/css" media="screen, projection">
+</head>
+<body>
+	<div id="logining_page">
+<div id="footer">
+	<a href="index.php"><img src="Custom_files/Logo_white_TM.png" width="20%" height="10%" id="logo"></a><a id="BigHeder">Bysec security technology trade mark</a><img src="Custom_files/g1.png" id="menu"><img src="Custom_files/g2.png" id="menu"><img src="Custom_files/g3.png" id="menu"><img src="Custom_files/g4.png" id="menu">	
+
+<div id="content" >
+<h1 id="headerBig"></h1>
+</div >
+<p id="InfoBox">© 2020 BYSEC</p>
+</div>
+
+</div>
+</body>
