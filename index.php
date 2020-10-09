dc583e78cf7831b8c91cb46e26b97c4bd030c7a3
Sirendium <Sirendium@gmail.com>
2020-10-09 03:29:42 +0300

    first commitfdf

--- index.php
+++ index.php
@@ -3,26 +3,32 @@
 <head>
 	<title>IQ Dashboard</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="Styles/background_interface.css" type="text/css" media="screen, projection">
+	<link rel="stylesheet" href="Styles/back_3.css" type="text/css" media="screen, projection">
 </head>
 <body>
 <div id="logining_page">
 <div id="footer">
-	<a href="index.php"><img src="Custom_files/Logo_white_TM.png" width="20%" height="10%" id="logo"></a><a id="BigHeder">Bysec security technology trade mark</a> 
-	<a  href="detects.php"><img src="Custom_files/g1.png" id="menu"></a>
-	<a href="monitoring.php"><img src="Custom_files/g2.png" id="menu"></a>
-	<a href="radar.php"><img src="Custom_files/g3.png" id="menu"></a>
-	<a href="IQ-WEB/index.php"><img src="Custom_files/g4.png" id="menu"></a>
+	<a href="index.php"><img src="Custom_files/Logo_white_TM.png" width="15%" height="5%" id="logo"></a>
+	<a  href="detects.php"><img src="Custom_files/g1.png" id="menu" width="3%"></a>
+	<a href="monitoring.php"><img src="Custom_files/g2.png" id="menu" width="3%"></a>
+	<a href="radar.php"><img src="Custom_files/g3.png" id="menu"width="3%"> </a>
+	<a href="IQ-WEB/index.php"><img src="Custom_files/g4.png" id="menu" width="3%"></a><br>
 <div id="content" >
-<form method="POST">
-	<h1 id="Zaglavie">Login</h1>
-	<a id="BigHeder">Username </a><input type="text" name="Username"><br>
-	<a id="BigHeder">Email  &#160; &#160; &#160; &#160; </a><input type="text" name="EMAIL"><br>
-	<a id="BigHeder">Password </a><input type="text" name="PASSWORD"><br><br>
-	<input type="submit" name="logining" value="Come to IQ DASHBOARD" id="button_login" >
+<form method="POST" id="Left_side">
+	<h1 id="Zaglavie">Login</h1>	<br>
+	<a id="BigHeder">Username</a><input type="text" name="Username"> <br>
+	<a id="BigHeder">Password&nbsp;</a><input type="text" name="PASSWORD"><br><br>
+	<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><input type="submit" name="logining" value="Enter" id="button_login" >
 </form>
-</div >
-<p id="InfoBox">© 2020 BYSEC</p>
+<img src="Custom_files/back3.png" width="40%" id="First_image">		</div>
+<div id="Left_side_1">
+		<img src="Custom_files/back1.png" width="30%" ><img src="Custom_files/back2.png" width="30%" id="center_img" >
+
+
+</div>
+
+
+<p id="InfoBox">© 2020 BYSEC SECURITY TRADEMARK</p>
 </div>
 
 </div>
